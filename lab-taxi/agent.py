7e70b76d2dba608ae01bffc666c679fe2cf68dc5
Kyungwon Chun <ruddyscent@gmail.com>
2021-03-10 23:20:30 +0900

    Solve mini project of lesson 10

--- lab-taxi/agent.py
+++ lab-taxi/agent.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from collections import defaultdict
 
@@ -12,6 +14,9 @@ class Agent:
         """
         self.nA = nA
         self.Q = defaultdict(lambda: np.zeros(self.nA))
+        self.eps = 0.005
+        self.gamma = 0.99
+        self.alpha = 0.1
 
     def select_action(self, state):
         """ Given the state, select an action.
@@ -24,7 +29,10 @@ class Agent:
         =======
         - action: an integer, compatible with the task's action space
         """
-        return np.random.choice(self.nA)
+        if random.random() > self.eps:
+            return np.argmax(self.Q[state])
+        else:
+            return np.random.choice(self.nA)
 
     def step(self, state, action, reward, next_state, done):
         """ Update the agent's knowledge, using the most recently sampled tuple.
@@ -37,4 +45,11 @@ class Agent:
         - next_state: the current state of the environment
         - done: whether the episode is complete (True or False)
         """
-        self.Q[state][action] += 1
+        current = self.Q[state][action]
+        policy_s = np.ones(self.nA) * self.eps / self.nA        # current policy (for next state S')
+        policy_s[np.argmax(self.Q[next_state])] += 1 - self.eps # greedy action
+        Qsa_next = np.dot(self.Q[next_state], policy_s)         # get value of state at next time step
+        target = reward + self.gamma * Qsa_next                 # construct target
+        new_value = current + self.alpha * (target - current)   # get updated value 
+    
+        self.Q[state][action] = new_value
